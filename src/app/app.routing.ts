e169ef37d9a09ccc3132f76c24e2a4e74565de4b
kittu <kritiketan7199@gmail.com>
2020-08-18 13:05:04 +1000

    profile updates:static

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,12 +10,12 @@ import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes =[
-    { path: 'home',             component: HomeComponent },
-    { path: 'user-profile',     component: ProfileComponent },
-    { path: 'register',           component: SignupComponent },
-    { path: 'landing',          component: LandingComponent },
-    { path: 'login',          component: LoginComponent },
-    { path: '', redirectTo: 'user-profile', pathMatch: 'full' }
+    //{ path: 'home',             component: HomeComponent },
+    { path: 'me',     component: ProfileComponent },
+    // { path: 'register',           component: SignupComponent },
+    // { path: 'landing',          component: LandingComponent },
+    // { path: 'login',          component: LoginComponent },
+    { path: '', redirectTo: 'me', pathMatch: 'full' }
 ];
 
 @NgModule({
